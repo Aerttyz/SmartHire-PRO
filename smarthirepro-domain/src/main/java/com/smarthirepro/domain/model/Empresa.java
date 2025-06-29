d3fd55dbd60c01d6c6d9201fb8e0a14098c53c88
Aerttyz <alesandrosilva2004@gmail.com>
2025-06-29 19:23:53 -0300

    feat: adicionar anotações de ID e geração para a classe Empresa

--- smarthirepro-domain/src/main/java/com/smarthirepro/domain/model/Empresa.java
+++ smarthirepro-domain/src/main/java/com/smarthirepro/domain/model/Empresa.java
@@ -7,6 +7,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +22,8 @@ import lombok.Setter;
 @Entity
 public class Empresa implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String cnpj;
     private String nome;
